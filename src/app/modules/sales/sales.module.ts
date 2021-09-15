d749cb228a8c05a2ba4dc2c965b8b8ea82519ca1
wisam87 <wisam878787@gmail.com>
2021-09-15 11:29:00 +0500

    header and layotu changes

--- src/app/modules/sales/sales.module.ts
+++ src/app/modules/sales/sales.module.ts
@@ -1,8 +1,11 @@
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SalesComponent } from './sales.component';
 import { RouterModule } from '@angular/router';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 @NgModule({
@@ -11,6 +14,10 @@ import { RouterModule } from '@angular/router';
     ],
     imports: [
         CommonModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSidenavModule,
+        MatProgressBarModule,
         RouterModule.forChild([{path: '', component: SalesComponent}])
     ]
 })
